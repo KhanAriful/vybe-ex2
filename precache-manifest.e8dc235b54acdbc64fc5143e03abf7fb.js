b3c0cda3019f73914a60ecaf1093a4aa0b184bbe
Ariful <ank4219@gmail.com>
2020-07-04 22:24:04 +0200

    deploy

---
+++ precache-manifest.e8dc235b54acdbc64fc5143e03abf7fb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4bbf02c0efddaefbafd4f128397b3d0",
+    "url": "/vybe-ex2/index.html"
+  },
+  {
+    "revision": "8bd777b47932516b6304",
+    "url": "/vybe-ex2/static/css/2.53f53a9f.chunk.css"
+  },
+  {
+    "revision": "485e2212411954d844fe",
+    "url": "/vybe-ex2/static/css/main.7efe0d38.chunk.css"
+  },
+  {
+    "revision": "8bd777b47932516b6304",
+    "url": "/vybe-ex2/static/js/2.8d1425f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vybe-ex2/static/js/2.8d1425f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "485e2212411954d844fe",
+    "url": "/vybe-ex2/static/js/main.0b4b4e16.chunk.js"
+  },
+  {
+    "revision": "b1078159732ef8eab435",
+    "url": "/vybe-ex2/static/js/runtime-main.8b916a2d.js"
+  },
+  {
+    "revision": "6abf597a1f3f44fd10bdcb7b86b494f0",
+    "url": "/vybe-ex2/static/media/Nunito-Black.6abf597a.ttf"
+  },
+  {
+    "revision": "1cd294a771f26752bbb8d8d5210f6412",
+    "url": "/vybe-ex2/static/media/Nunito-Bold.1cd294a7.ttf"
+  },
+  {
+    "revision": "19caf1c06e022593ea4657734d691c86",
+    "url": "/vybe-ex2/static/media/Nunito-ExtraBold.19caf1c0.ttf"
+  },
+  {
+    "revision": "76ae20d4d4b7d46e613d2581512fcf73",
+    "url": "/vybe-ex2/static/media/Nunito-SemiBold.76ae20d4.ttf"
+  },
+  {
+    "revision": "7a9f18435edbcc3d10758778ca51bdb9",
+    "url": "/vybe-ex2/static/media/icomoon.7a9f1843.ttf"
+  }
+]);
